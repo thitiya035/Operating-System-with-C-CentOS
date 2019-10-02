152e5501a54f9cadcda1da3e0af8c9185fffec06
weerayut-PC <weerayut.fame@gmail.com>
2019-10-02 07:57:49 +0700

    update lab

--- Lab3_Fork/ex1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h> /* contains fork prototype */
-int main(void){
-	printf("Hello World!\n");
-	fork( );
-	printf("I am after forking\n");
-	printf("\tI am process %d.\n", getpid( ));
-}
